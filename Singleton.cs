e0c5dd2f278f4c74a4a21ddcdea4d7926139bc28
Uktena-34 <dgomezamaton9@gmail.com>
2023-10-27 17:46:14 -0600

    Agregar archivos de proyecto.

---
+++ Singleton.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.PerformanceData;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    public class Singleton
+    {
+        private static Singleton instance = null;
+        public string mensaje = " ";
+        protected Singleton() 
+        {
+            mensaje = "HOLA MUNDO";
+        }
+        public static Singleton Instance 
+        {
+            get
+            {
+                if (instance == null)
+                    instance = new Singleton();
+
+                return instance;
+                
+
+            }
+        }
+
+
+    }
+    
+}
